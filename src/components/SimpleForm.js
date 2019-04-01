ee0f6a0fc956bd71efbb45f6b08a506824392528
Tobias Lehwalder <tobias.lehwalder@cosee.biz>
2019-04-01 21:58:54 +0200

    remove spread

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,67 +1,88 @@
-import React from 'react';
+import React from "react";
 import Input from "./Input";
 
 export default class SimpleForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onCityChange = this.onCityChange.bind(this);
+    this.onAgeChange = this.onAgeChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onClear = this.onClear.bind(this);
 
-        this.onNameChange = this.onNameChange.bind(this);
-        this.onCityChange = this.onCityChange.bind(this);
-        this.onAgeChange = this.onAgeChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onClear = this.onClear.bind(this);
+    this.state = {
+      name: "",
+      age: "",
+      city: ""
+    };
+  }
 
-        this.state = {
-            name: "",
-            age: "",
-            city: ""
-        }
-    }
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
 
-    onNameChange(event) {
-        this.setState({...this.state, name: event.target.value})
-    }
+  onCityChange(event) {
+    this.setState({ city: event.target.value });
+  }
 
-    onCityChange(event) {
-        this.setState({...this.state, city: event.target.value})
-    }
+  onAgeChange(event) {
+    this.setState({ age: event.target.value });
+  }
 
-    onAgeChange(event) {
-        this.setState({...this.state, age: event.target.value})
-    }
+  onSubmit(event) {
+    event.preventDefault();
 
-    onSubmit(event) {
-        event.preventDefault();
-
-        const formOutput = `Name: ${this.state.name}
+    const formOutput = `Name: ${this.state.name}
 Wohnort: ${this.state.city}
 Jahre: ${this.state.age}`;
 
-        const description = (this.state.name && this.state.city && this.state.age) ? `\n\n${this.state.name} ist ${this.state.age} Jahre alt und kommt aus ${this.state.city}` : '';
+    const description =
+      this.state.name && this.state.city && this.state.age
+        ? `\n\n${this.state.name} ist ${
+            this.state.age
+          } Jahre alt und kommt aus ${this.state.city}`
+        : "";
 
-        alert(formOutput + description)
-    }
+    alert(formOutput + description);
+  }
 
-    onClear(event) {
-        event.preventDefault();
-       this.setState({name: '', city: '', age: ''})
-    }
+  onClear(event) {
+    event.preventDefault();
+    this.setState({ name: "", city: "", age: "" });
+  }
 
-    render() {
-        return <>
-            <h1>My React Form</h1>
-            <form onSubmit={this.onSubmit}>
+  render() {
+    return (
+      <>
+        <h1>My React Form</h1>
+        <form onSubmit={this.onSubmit}>
+          <Input
+            title="Name"
+            type="text"
+            handleChange={this.onNameChange}
+            value={this.state.name}
+            placeholder={"Namen eingeben"}
+          />
+          <Input
+            title="Wohnort"
+            type="text"
+            handleChange={this.onCityChange}
+            value={this.state.city}
+            placeholder={"Wohnort eingeben"}
+          />
+          <Input
+            title="Alter"
+            type="text"
+            handleChange={this.onAgeChange}
+            value={this.state.age}
+            placeholder={"Alter eingeben"}
+          />
 
-                <Input title="Name" type="text" handleChange={this.onNameChange} value={this.state.name} placeholder={"Namen eingeben"}/>
-                <Input title="Wohnort" type="text" handleChange={this.onCityChange} value={this.state.city}  placeholder={"Wohnort eingeben"}/>
-                <Input title="Alter" type="text" handleChange={this.onAgeChange} value={this.state.age}  placeholder={"Alter eingeben"}/>
-
-                <button onClick={this.onSubmit}>Submit</button>
-                <button onClick={this.onClear}>Clear</button>
-            </form>
-        </>
-
-    }
-
+          <button onClick={this.onSubmit}>Submit</button>
+          <button onClick={this.onClear}>Clear</button>
+        </form>
+      </>
+    );
+  }
 }
