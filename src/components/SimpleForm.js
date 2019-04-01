9423c3384de078c56711776910417e90404bfb3f
Tobias Lehwalder <tobias.lehwalder@cosee.biz>
2019-04-01 22:21:33 +0200

    simpler

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,42 +1,27 @@
-import React from 'react';
+import React from "react";
 
 export default class SimpleForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-        this.onSubmit = this.onSubmit.bind(this);
+  onSubmit(event) {
+    event.preventDefault();
+  }
 
-        this.state = {
-            //initialen State anlegen
-        }
-    }
+  render() {
+    return (
+      <>
+        <h1>My React Form</h1>
+        <form onSubmit={this.onSubmit}>
+          {/* Inputs mit Labels hier einfügen*/}
 
-
-    onSubmit(event) {
-        event.preventDefault();
-
-        const formOutput = `Name: ${this.state.name}
-Wohnort: ${this.state.city}
-Jahre: ${this.state.age}`;
-
-        const description = (this.state.name && this.state.city && this.state.age) ? `\n\n${this.state.name} ist ${this.state.age} Jahre alt und kommt aus ${this.state.city}` : '';
-
-        alert(formOutput + description)
-    }
-
-    render() {
-        return <>
-            <h1>My React Form</h1>
-            <form onSubmit={this.onSubmit}>
-
-                {/* Inputs mit Labels hier einfügen*/}
-
-                <button onClick={this.onSubmit}>Submit</button>
-                {/* Clear Button einfügen */}
-            </form>
-        </>
-
-    }
-
+          <button onClick={this.onSubmit}>Submit</button>
+          {/* Clear Button einfügen */}
+        </form>
+      </>
+    );
+  }
 }
