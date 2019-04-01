fe1c20e6f7e09edb9197e2521f3fe4edac1da986
Tobias Lehwalder <tobias.lehwalder@cosee.biz>
2019-04-01 22:50:09 +0200

    data

---
+++ src/data/articles.js
@@ -0,0 +1,45 @@
+export const articles = [
+  {
+    title: "Jane goes swimming",
+    description: "Lorem ipsum, some text",
+    tags: ["news", "sports", "comedy"],
+    author: "Mary"
+  },
+  {
+    title: "Cool Title",
+    description: "Lorem ipsum, some more text",
+    tags: ["news", "nonsense", "comedy"],
+    author: "Franky"
+  },
+  {
+    title: "Eintracht Frankfurt wins Euroleague",
+    description: "Lorem ipsum, some more text",
+    tags: ["sports"],
+    author: "Tobi"
+  },
+  {
+    title: "Breaking NEWS!",
+    description: "Lorem ipsum, lorem lorem lorem",
+    tags: ["news", "breaking"],
+    author: "Johnny"
+  },
+  {
+    title: "Some Title",
+    description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+    tags: ["nonsense", "comedy"],
+    author: "Peter"
+  },
+  {
+    title: "Learning React",
+    description: "Lorem ipsum, some more text, lorem ipsum ipsum ipsum ipsum",
+    tags: ["dev"],
+    author: "Brendan"
+  },
+  {
+    title: "Toffifee <3",
+    description:
+      "Lorem ipsum, some more text, ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum",
+    tags: ["romantic", "comedy"],
+    author: "Mo"
+  }
+];
